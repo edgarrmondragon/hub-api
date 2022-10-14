4e5b9fed192e6ae6f80d1aabbfb8fc6137a8de8b
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-10-14 02:38:44 -0500

    Commit test fixtures

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,20 +1,23 @@
 """Test FastAPI app."""
 
 from fastapi.testclient import TestClient
+from pytest import MonkeyPatch
 
 from app.main import app
 
 
-def test_plugin_index():
+def test_plugin_index(monkeypatch: MonkeyPatch):
     """Test app."""
+    monkeypatch.setenv("PLUGINS_FILES_PATH", "tests/fixtures/plugins")
     client = TestClient(app)
     response = client.get("/meltano/api/v1/plugins/extractors/index")
     assert response.status_code == 200
     assert response.json()
 
 
-def test_plugin_details():
+def test_plugin_details(monkeypatch: MonkeyPatch):
     """Test app."""
+    monkeypatch.setenv("PLUGINS_FILES_PATH", "tests/fixtures/plugins")
     client = TestClient(app)
     response = client.get("/meltano/api/v1/plugins/extractors/tap-github--singer-io")
     assert response.status_code == 200
