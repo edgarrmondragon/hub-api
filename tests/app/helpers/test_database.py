b70c541dea78b8d15553f3de73e8280ea69c937b
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-10-14 02:28:48 -0500

    Fix package name and tests

--- tests/app/helpers/test_database.py
+++ tests/app/helpers/test_database.py
@@ -1,13 +1,15 @@
 """Test database helper functions."""
 
+from contextlib import asynccontextmanager
+
 import pytest
 
 from app.helpers.database import get_db
 
 
 @pytest.mark.asyncio
-async def test_get_db():
+async def test_db():
     """Test get_db."""
-    db = yield await get_db()
-    async with db.execute("SELECT * FROM foo") as cursor:
+    _get_db = asynccontextmanager(get_db)
+    async with _get_db() as db, db.execute("SELECT * FROM foo") as cursor:
         assert await cursor.fetchall() == [{"bar": 42, "baz": "hello"}]
