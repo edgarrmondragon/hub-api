a923ca6cabebdc37f64030ddb6080a0d8a57569c
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-09-11 22:32:14 -0600

    Use ETag to cache responses on the client

---
+++ src/hub_api/etag.py
@@ -0,0 +1,44 @@
+"""ETag implementation.
+
+This combination of FastAPI middleware and dependency will add an ETag header to all responses.
+The ETag value is the version of the hub-api package. The incoming request's If-None-Match
+header is compared to the ETag value. If they match, a 304 Not Modified response is returned.
+Otherwise, the response is returned as normal.
+
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
+"""
+
+import typing as t
+from importlib import metadata
+
+import fastapi.middleware
+from fastapi import Header, Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+def get_etag() -> str:
+    """Get ETag value."""
+    return f"etag-{metadata.version("hub-api")}"
+
+
+class ETagMiddleware(BaseHTTPMiddleware):
+    """ETag middleware."""
+
+    async def dispatch(
+        self,
+        request: Request,
+        call_next: t.Callable[[Request], t.Awaitable[Response]],
+    ) -> fastapi.Response:
+        """Add ETag header to response."""
+        response = await call_next(request)
+        response.headers["ETag"] = get_etag()
+        return response
+
+
+class CheckETag:
+    """ETag dependency."""
+
+    def __call__(self, if_none_match: t.Annotated[str | None, Header()] = None) -> None:
+        """Get ETag value."""
+        if if_none_match == get_etag():
+            raise fastapi.HTTPException(status_code=304)
