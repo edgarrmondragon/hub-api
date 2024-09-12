1701c2c677d994535bd30c752df32f8d84a566fa
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-09-11 22:39:59 -0600

    Add test for ETag

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -2,10 +2,12 @@
 
 from __future__ import annotations
 
+import http
+
 import httpx
 import pytest
 
-from hub_api import enums, main
+from hub_api import enums, etag, main
 
 
 @pytest.fixture(scope="session")
@@ -18,7 +20,7 @@ def api() -> httpx.AsyncClient:
 async def test_plugin_index(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/plugins/extractors/index."""
     response = await api.get("/meltano/api/v1/plugins/index")
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
     assert response.json()
 
 
@@ -26,7 +28,7 @@ async def test_plugin_index(api: httpx.AsyncClient) -> None:
 async def test_plugin_type_index(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/plugins/extractors/index."""
     response = await api.get("/meltano/api/v1/plugins/extractors/index")
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
     assert response.json()
 
 
@@ -50,17 +52,28 @@ async def test_plugin_details(
     """Test /meltano/api/v1/plugins/extractors/<plugin>--<variant>."""
     path = f"/meltano/api/v1/plugins/{plugin_type}/{plugin}--{variant}"
     response = await api.get(path)
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
 
     details = response.json()
     assert details["name"] == plugin
 
 
 @pytest.mark.asyncio
+async def test_plugin_details_etag_match(api: httpx.AsyncClient) -> None:
+    """Test /meltano/api/v1/plugins/stats."""
+    etag_value = etag.get_etag()
+    response = await api.get(
+        "/meltano/api/v1/plugins/extractors/index",
+        headers={"If-None-Match": etag_value},
+    )
+    assert response.status_code == http.HTTPStatus.NOT_MODIFIED
+
+
+@pytest.mark.asyncio
 async def test_hub_stats(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/plugins/stats."""
     response = await api.get("/meltano/api/v1/plugins/stats")
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
 
     stats = response.json()
     assert isinstance(stats["extractors"], int)
@@ -70,7 +83,7 @@ async def test_hub_stats(api: httpx.AsyncClient) -> None:
 async def test_maintainers(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/maintainers."""
     response = await api.get("/meltano/api/v1/maintainers", follow_redirects=True)
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
 
     maintainers = response.json()
     maintainer = next(filter(lambda m: m["id"] == "edgarrmondragon", maintainers))
@@ -82,7 +95,7 @@ async def test_maintainers(api: httpx.AsyncClient) -> None:
 async def test_maintainer_details(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/maintainers."""
     response = await api.get("/meltano/api/v1/maintainers/edgarrmondragon")
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
 
     maintainer = response.json()
     assert maintainer["id"] == "edgarrmondragon"
@@ -95,7 +108,7 @@ async def test_maintainer_details(api: httpx.AsyncClient) -> None:
 async def test_top_maintainers(api: httpx.AsyncClient) -> None:
     """Test /meltano/api/v1/maintainers."""
     response = await api.get("/meltano/api/v1/maintainers/top/3")
-    assert response.status_code == 200
+    assert response.status_code == http.HTTPStatus.OK
 
     maintainers = response.json()
     assert len(maintainers) == 3
