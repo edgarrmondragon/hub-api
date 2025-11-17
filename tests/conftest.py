8541e7c43e4fd07a250c6a8820b421d9fd9a1ab4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-11-17 17:21:37 -0600

    Report whether a free-threaded build is being used in tests

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import sysconfig
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pytest
+
+
+def pytest_report_header(config: pytest.Config) -> list[str]:  # noqa: ARG001
+    """Return a list of strings to be displayed in the header of the report."""
+    is_freethreaded = bool(sysconfig.get_config_var("Py_GIL_DISABLED"))
+
+    return [
+        f"Free-threaded: {is_freethreaded}",
+    ]
